75163a7d138207fe129e8dfe2aae4b3aded01396
Adrianna Janik <ada.janik@gmail.com>
2019-03-21 01:20:47 -0400

    commit init

---
+++ scatter.js
@@ -0,0 +1,77 @@
+/**
+ * Experiment Visualizing Errors in Activation Space
+ */
+
+var points = d3.select("#points"),
+    heatmap = d3.select("#heatmap"),
+    masks = d3.select("#masks"),
+    preds = d3.select("#preds"),
+    patches = d3.select("#patches");
+
+var scales = {
+  "x": d3.scaleLinear()
+    .domain([-16, 16])
+    .range([0, 2000]),
+  "y": d3.scaleLinear()
+    .domain([-16, 16])
+    .range([0, 1000]),
+  "color": d3.scaleSequential(d3.interpolateRdBu)//PiYG)
+    .domain([0, 1])
+};
+
+
+var svg = d3.select("#svg-quant");
+
+svg.append("g")
+  .attr("class", "legendLinear")
+  .attr("transform", "translate(132,900)");
+
+var legendLinear = d3.legendColor()
+  .shapeWidth(35)
+  .shapeHeight(30)
+  .cells([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+  .orient('horizontal')
+  .scale(scales.color);
+
+svg.select(".legendLinear")
+  .call(legendLinear);
+
+// draw the scatterplot points
+points.selectAll("circle")
+  .data(points_data)
+  .enter()
+  .append("circle")
+  .attrs({
+    "cx": (d) => scales.x(d.coords[0]),
+    "cy": (d) => scales.y(d.coords[1]),
+    "r": 3,
+    "fill": (d) => scales.color(d.iou),
+    "stroke": "black",
+    "stroke-width": 1
+  });
+
+// draw the background heatmap
+var ux = [... new Set(hm_data.map((d) => d.coords[0]))],
+    uy = [... new Set(hm_data.map((d) => d.coords[1]))];
+
+
+heatmap.selectAll("rect")
+  .data(hm_data).enter()
+  .append("rect")
+  .attrs({
+    "x": (d) => scales.x(d.coords[0]),
+    "y": (d) => scales.y(d.coords[1]),
+    "width": scales.x(ux[1]) - scales.x(ux[0]),
+    "height": scales.y(uy[1]) - scales.y(uy[0]),
+    "fill": (d) => scales.color(d.iou),
+    "opacity": 0.7
+  });
+
+
+// allows interactivity with points
+var brush = d3.brush()
+    .extent([[140, 100], [1850, 900]])
+    .on("start brush end", brushed);
+d3.select("#brush").call(brush);
+
+
---
+++ scatter_funs.js
@@ -0,0 +1,76 @@
+/**
+ * Functions for drawing the scatterplot
+ */
+
+
+function update_patches(im_ids) {
+  var patch_sel = patches.selectAll(".patch")
+      .data(im_ids, (d) => d);
+
+  patch_sel.enter()
+    .append("svg:image")
+    .attrs({
+      "x": 1800,
+      "height": 100,
+      "width": 100,
+      "xlink:href": (d) => d
+    })
+    .classed("patch", true);
+
+  patch_sel.attr("y", (d, i) => (100 + 10) * i + 100);
+  patch_sel.exit().remove();
+}
+
+function update_masks(im_ids) {
+  var mask_sel = masks.selectAll(".mask")
+      .data(im_ids, (d) => d);
+
+  mask_sel.enter()
+    .append("svg:image")
+    .attrs({
+      "x": 1910,
+      "height": 100,
+      "width": 100,
+      "xlink:href": (d) => d
+    })
+    .classed("mask", true);
+
+  mask_sel.attr("y", (d, i) => (100 + 10) * i + 100);
+  mask_sel.exit().remove();
+}
+
+
+function update_preds(im_ids) {
+  var pred_sel = preds.selectAll(".pred")
+      .data(im_ids, (d) => d);
+
+  pred_sel.enter()
+    .append("svg:image")
+    .attrs({
+      "x": 2020,
+      "height": 100,
+      "width": 100,
+      "xlink:href": (d) => d
+    })
+    .classed("pred", true);
+
+  pred_sel.attr("y", (d, i) => (100 + 10) * i + 100);
+  pred_sel.exit().remove();
+}
+
+function brushed() {
+  var value = [];
+  if (d3.event.selection) {
+    var [[x0, y0], [x1, y1]] = d3.event.selection;
+    var selected = points_data.filter(
+      d => x0 <= scales.x(d.coords[0]) && scales.x(d.coords[0]) < x1 &&
+        y0 <= scales.y(d.coords[1]) && scales.y(d.coords[1]) < y1
+    ).slice(0,8);
+  }
+  update_patches(selected.map((d) => d.patch_path));
+  update_masks(selected.map((d) => d.mask_path));
+  update_preds(selected.map((d) => d.pred_path));
+
+}
+
+
